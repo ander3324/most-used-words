8be6a4a4be166fbb7c1d75531ee889dc1045bbe9
Ander <ander@localhost.localdomain>
2020-10-08 23:34:59 -0300

    First commit

---
+++ src/components/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container fluid>
+    <v-form>
+      <v-file-input
+        multiple
+        chips
+        v-model="files"
+        label="Select subtitle files"
+        append-outer-icon="mdi-send"
+        prepend-icon="mdi-message-text"
+        outlined
+        @click:append-outer="processSubtitles"
+      />
+    </v-form>
+
+    <div class="pills">
+      <Pill
+        v-for="word in groupedWords"
+        :key="word.name"
+        :name="word.name"
+        :amount="word.amount"
+      />
+    </div>
+  </v-container>
+</template>
+
+<script>
+import Pill from "./Pill";
+
+export default {
+  components: { Pill },
+  data: function () {
+    return {
+      files: [],
+      groupedWords: [
+        { name: "i", amount: 1243 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 },
+      ],
+    };
+  },
+  methods: {
+    processSubtitles() {
+      console.log(this.files);
+    },
+  },
+};
+</script>
+
+<style>
+.pills {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
